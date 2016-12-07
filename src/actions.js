76a2a17886372645cd820139071f978db5a4de4a
Daniel <danbernardi1@gmail.com>
2016-12-07 12:12:28 -0800

    adds navigation reducers

--- src/actions.js
+++ src/actions.js
@@ -6,6 +6,14 @@ export const addPreviousSheetID = (sheetID) => ({
   type: 'ADD_PREVIOUS_SHEET_ID', sheetID
 });
 
+export const resetPreviousHistory = () => ({
+  type: 'RESET_PREVIOUS_HISTORY'
+});
+
 export const revertToPreviousSheetID = () => ({
   type: 'REVERT_TO_PREVIOUS_SHEET_ID'
+});
+
+export const setAnswerToQuestion = (sheetID, answer) => ({
+  type: 'SET_ANSWER_TO_QUESTION', sheetID, answer
 });
--- src/reducers.js
+++ src/reducers.js
@@ -1,7 +1,9 @@
-import { combineReducers } from 'redux';  
+import { combineReducers } from 'redux';
+import _ from 'lodash';
 
 const initialState = {
-  activeSheetID: { current: 'index', previous: [] }
+  activeSheetID: { current: 'index', previous: [] },
+  questionAnswerPairs: []
 };
 
 const activeSheetID = (state = initialState.activeSheetID, action) => {
@@ -16,6 +18,10 @@ const activeSheetID = (state = initialState.activeSheetID, action) => {
       return Object.assign({}, state, { previous: previousArray });
     }
 
+    case 'RESET_PREVIOUS_HISTORY': {
+      return Object.assign({}, state, { previous: [] });
+    }
+
     case 'REVERT_TO_PREVIOUS_SHEET_ID': {
       const previousArray = state.previous;
       return Object.assign({}, state, { current: previousArray.pop(), previous: previousArray })
@@ -27,8 +33,30 @@ const activeSheetID = (state = initialState.activeSheetID, action) => {
   }
 };
 
+const questionAnswerPairs = (state = initialState.questionAnswerPairs, action) => {
+  switch(action.type) {
+    case 'SET_ANSWER_TO_QUESTION': {
+      const newState = state.slice();
+
+      if (newState.filter(a => a.questionID === action.sheetID).length) {
+        const existingQuestionIndex = _.findIndex(newState, (a) => a.questionID === action.sheetID);
+        newState[existingQuestionIndex] = { questionID: action.sheetID, answer: action.answer };
+      } else {
+        newState.push({ questionID: action.sheetID, answer: action.answer }); 
+      }
+
+      return newState;
+    }
+
+    default: {
+      return state;
+    }
+  }
+}
+
 const reducers = combineReducers({
-  activeSheetID
+  activeSheetID,
+  questionAnswerPairs
 });
 
 export default reducers;
