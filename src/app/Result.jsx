a3acc2068977db222595442b0162bac0c287869e
Sasha Klein <sashafklein@gmail.com>
2016-12-16 10:25:22 -0800

    Removed ineffective debugger

--- src/app/Result.jsx
+++ src/app/Result.jsx
@@ -118,12 +118,9 @@ Index.propTypes = {
   questionAnswerPairs: React.PropTypes.array
 };
 
-const injectStateProps = state => {
-  window.state = state;
-  return {
-    activeSheetID: state.activeSheetID,
-    questionAnswerPairs: state.questionAnswerPairs
-  };
-}
+const injectStateProps = state => ({
+  activeSheetID: state.activeSheetID,
+  questionAnswerPairs: state.questionAnswerPairs
+});
 
 export default connect(injectStateProps)(Index);
