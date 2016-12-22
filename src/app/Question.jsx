7d9dcd2670bee64faf922b8804ba8aaf2c36c445
AndrewFitzpatrick <andrew@weareredshift.com>
2016-12-21 16:57:06 -0800

    further adventures of QA

--- src/app/Question.jsx
+++ src/app/Question.jsx
@@ -23,7 +23,6 @@ class Question extends React.Component {
 
   respondOrSwitchPages(targetID) {
     const { selectedAnswer } = this.state;
-
     if (!selectedAnswer) {
       return null;
     }
@@ -51,7 +50,7 @@ class Question extends React.Component {
     const callbacks = [];
 
     // Fill array of sequential actions to fade out question, show responses, then move
-    this.setState({ hideQuestion: true }); // Fade out old
+    this.setState({ hideQuestion: true,  }); // Fade out old
 
     responses.forEach(response => {
       callbacks.push(() => {
--- src/app/components/RangeSlider.jsx
+++ src/app/components/RangeSlider.jsx
@@ -22,7 +22,7 @@ const RangeSlider  = props => {
       <div className="rangeslider flex-fields">
         <span className="flex-inline rangeslider-label layout--abs_left">{ range[0] }</span>
         <input className="input__range_slider" type="range" min="1" max="9" defaultValue="5" step="1" onChange={ (event) => dispatchValue(event) } />
-        <span className="flex-inline rangeslider-label layout--abs_right">{ range[1] }</span>
+        <span className="flex-inline rangeslider-label layout--abs_right typ--left">{ range[1] }</span>
       </div>
       <div className="tick-fields flex-fields">
         <span className="flex-inline rangeslider-label layout--abs_left">{ range[0] }</span>
@@ -37,7 +37,7 @@ const RangeSlider  = props => {
           <div className="tick-mark"></div>
           <div className="tick-mark"></div>
         </div>
-        <span className="flex-inline rangeslider-label layout--abs_right">{ range[1] }</span>
+        <span className="flex-inline rangeslider-label layout--abs_right typ--left">{ range[1] }</span>
       </div>
     </div>
   );
