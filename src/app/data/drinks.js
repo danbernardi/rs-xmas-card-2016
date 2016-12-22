aba2b717f093a34952b6c49c8e3c16b18d4ab905
AndrewFitzpatrick <andrew@weareredshift.com>
2016-12-22 11:44:04 -0800

    Thursday QA

--- src/app/data/drinks.js
+++ src/app/data/drinks.js
@@ -8,9 +8,9 @@ export const drinks = [
     description: "For what some call the evolutionary link between the Manhattan and the Martini, the Martinez came from humble beginnings. As the story goes, a traveller came into a San Francisco bar and asked for a stout drink before his trip to Martinez, a town north of the city. The bartender reached for Old Tom Gin, which, at the time, was a “poorly distilled gin with sugar added so it’d be palatable.” This sweetened gin became the signature ingredient of this robust yet balanced sipping cocktail.",
     ingredients: [
       '1½ ounces Old Tom gin',
-      'Sweet vermouth',
+      'Sweet Vermouth',
       '¼ ounce Luxardo Maraschino liqueur',
-      '2 dashses Angostura bitters'
+      '2 dashes Angostura bitters'
     ],
     instructions: [
       'Add all ingredients to a shaker and fill with ice.',
@@ -35,10 +35,10 @@ export const drinks = [
     color: '#66385C',
     description: "“Remember the Maine, to hell with Spain!” screamed warmongering journalists when the USS Maine exploded in the Havana Harbor in 1898 under mysterious circumstances. But when famed spirits writer Charles H. Baker first told of this drink, a rye-based number that draws comparisons to both the Sazerac and Manhattan, he was speaking in context of the 1933 Cuban revolution. At the time, he sat in a hotel bar in Havana, “when each swallow was punctuated with bombs going off on the Prado.”",
     ingredients: [
-      '2 ounces rye',
-      '¾ ounce sweet vermouth',
+      '2 ounces Rye',
+      '¾ ounce Sweet Vermouth',
       '2 teaspoons Cherry Heering',
-      '1 dash absinthe'
+      '1 dash Absinthe'
     ],
     instructions: [
       'In a chilled coupe or cocktail glass, add a dash of absinthe. Roll around to coat and discard excess.',
@@ -67,8 +67,8 @@ export const drinks = [
     description: "Like so many cocktails, the Jack Rose is embodied by the colorful characters who sipped it — from prohibition-era gangsters, to author John Steinbeck, to Hemingway’s wounded, languishing narrator in “The Sun Also Rises”. The drink is a sour made from apple brandy — “applejack” as it was known then — and it remains a simple but solid addition to the canon.",
     ingredients: [
       '1 ounce fresh lemon juice',
-      '½ ounce grenadine',
-      '2 ounces applejack'
+      '½ ounce Grenadine',
+      '2 ounces Applejack'
     ],
     instructions: [
       'Shake all ingredients well with ice.',
@@ -116,9 +116,9 @@ export const drinks = [
     heading: "You’re a smooth operator with swagger and style. This confident, young cocktail is the perfect holiday wingman.",
     img: 'paper_plane.png',
     color: '#385A24',
-    description: "With four equal parts bourbon, aperol, amaro, and lemon juice — this <em>aperitivo</em> is a cinch to make and a pleasure to drink. If you like sour-forward cocktails, you’ll find this drink immediately likeable. And yes, its name is a reference to MIA’s hit song “Paper Planes”.",
+    description: "With four equal parts bourbon, aperol, amaro, and lemon juice — this <em>aperitivo</em> is a cinch to make and a pleasure to drink. If you like sour-forward cocktails, you’ll find this drink immediately likeable. And yes, its name is a reference to MIA’s hit song “Paper Planes.”",
     ingredients: [
-      '¾ ounces bourbon',
+      '¾ ounces Bourbon',
       '¾ ounces Aperol',
       '¾ ounces Amaro Nonino',
       '¾ ounces lemon juice'
@@ -143,7 +143,7 @@ export const drinks = [
     color: '#243C56',
     description: "The gin that gangsters smuggled into the South Side of Chicago had a harsh bite, requiring cocktails with sweeter mixers than their North Side counterparts. At least that’s the story surrounding Al Capone and his alleged drink of choice — the South Side — a gin-based number with lime juice, simple syrup, and sprigs of mint. The East Side does something similar but with the added brightness of St. Germain and cucumber.",
     ingredients: [
-      '2 ounces Plymouth gin',
+      '2 ounces Plymouth Gin',
       '1 ounce St Germain',
       '¾ ounce Fresh lime juice'
     ],
@@ -172,8 +172,8 @@ export const drinks = [
     color: '#6B3C1D',
     description: 'Swap whiskey with tequila and mezcal for this smokey and strong yet subtly balanced South-of-the-Border riff on an old classic. Invented by Philip Ward in the East Village in 2007, the instant popularity of the Oaxaca Old Fashioned helped lead to a resurgence in cocktails featuring Mexican spirits.',
     ingredients: [
-      '1½ ounces reposado tequila',
-      '½ ounce mezcal',
+      '1½ ounces Reposado Tequila',
+      '½ ounce Mezcal',
       '1 teaspoon agave nectar',
       '1 dash Angostura bitters'
     ],
@@ -202,7 +202,7 @@ export const drinks = [
     ingredients: [
       '¾ ounces The Glenrothes Select',
       'Reserve Single Malt Scotch Whisky',
-      '¾ ounces Sweet vermouth',
+      '¾ ounces Sweet Vermouth',
       '¾ ounces Cherry Heering',
       '¾ ounces orange juice'
     ],
@@ -240,7 +240,7 @@ export const drinks = [
     instructions: [
       'Gently bruise the mint with lime juice in a shaker using a muddler or wooden spoon.',
       'Add syrup, rum, bitters and a big handful of ice, and shake until well chilled.',
-      'Strain into a coupe or cocktail glass and add Champagne',
+      'Strain into a coupe or cocktail glass and add Champagne.',
       'With a knife, slit the lime slice halfway through and pierce it with the mint sprig. Perch it on the rim of the glass.'
     ],
     garnish: [
@@ -263,10 +263,10 @@ export const drinks = [
     color: '#D56F22',
     description: 'A strong but dangerously drinkable tiki drink created by legendary Don the Beachcomber after World War II, this cocktail gets its namesake from the morse code signal for “Victory”: three dots and a dash. Its name is cleverly reflected in the garnish of three maraschino cherries on a cocktail pick (the dots) and one pineapple frond (the dash).',
     ingredients: [
-      '1 ounce rhum agricole (preferably Duquesne Agricole Eleve Sous Bois)',
-      '1 ounce rum, Guyanese (preferably El Dorado 5 Year)',
-      '½ ounce dry curaçao',
-      '½ ounce falernum',
+      '1 ounce Rhum Agricole (preferably Duquesne Agricole Eleve Sous Bois)',
+      '1 ounce Rum, Guyanese (preferably El Dorado 5 Year)',
+      '½ ounce dry Curaçao',
+      '½ ounce Falernum',
       '¼ ounce St. Elizabeth Allspice Dram',
       '1 ounce lime juice',
       '½ ounce honey syrup (2:1, honey:water)',
@@ -300,11 +300,11 @@ export const drinks = [
     ingredients: [
       '½ ounce fresh lemon juice',
       '1½ ounce fresh orange juice',
-      '½ ounce orgeat',
+      '½ ounce Orgeat',
       '1 ounce Pisco',
-      '½ ounce gin',
+      '½ ounce Gin',
       '2 ounces blended light aged rum',
-      '½ ounce oloroso sherry'
+      '½ ounce Oloroso sherry'
     ],
     instructions: [
       'Fill a cocktail shaker with ice.',
@@ -313,7 +313,7 @@ export const drinks = [
       'Garnish with the mint sprig.'
     ],
     garnish: [
-      '1 mint sprig'
+      'mint sprig'
     ],
     scores: [7, 4, 5],
     music: {
@@ -332,10 +332,10 @@ export const drinks = [
     description: 'A hungover customer walked into the legendary Don the Beachcomber’s tiki bar and asked for something to help him through his business travels. Several days later, the customer returned rubbing his head, complaining that Don’s concoction had turned him into a zombie for the rest of the trip. And thus, bleary-eyed and troublemaking, the Zombie was born. The drink’s fun and fruity tenor belies a potency that has forced Beachcomber restaurants to instill a two-Zombie-per-person limit.',
     ingredients: [
       '¾ ounce fresh lime juice',
-      '½ ounce felernum',
-      '1½ ounces each gold Puerto Rican rum and gold or dark Jamaican rum',
-      '1 ounce 151-proof Lemon Hard Demerara rum',
-      '1 teaspoon grenadine',
+      '½ ounce Falernum',
+      '1½ ounces each gold Puerto Rican rum and gold or dark Jamaican Rum',
+      '1 ounce 151-proof Lemon Hard Demerara Rum',
+      '1 teaspoon Grenadine',
       '6 drops Pernod',
       '1 dash of Angostura bitters',
       '½ ounce Don’s mix (2 parts grapefruit juice to 1 part cinnamon-infused sugar syrup blended at high speed with 6 ounces (¾ cup) crushed ice for no more than 5 seconds)',
